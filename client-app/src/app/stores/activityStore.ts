2c5e1d0a1f17eb3ba2873628a9870cc47263ece4
Chien-Shan Peng <crystalline@crystalline.work>
2024-08-25 23:45:39 -0700

    sec7: Refactoring the app to use MobX

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,31 @@
 import { makeAutoObservable } from "mobx";
+import { Activity } from "../models/activity";
+import agent from "../api/agent";
 
 export default class ActivityStore {
-    title = "Hello from MobX!";
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this);
     }
 
-    setTitle = () => {
-        this.title = this.title + '!';
+    loadActivities = async () => {
+        this.loadingInitial = true;
+
+        try {
+            const activities = await agent.Activities.list();
+            activities.forEach(act => {
+                act.date = act.date.split('T')[0];
+                this.activities.push(act);
+                this.loadingInitial = false;
+            });
+        } catch (error) {
+            console.log(error);
+            this.loadingInitial = false;
+        }
     }
 }
